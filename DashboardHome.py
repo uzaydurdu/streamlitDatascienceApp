3e9b9e23032f465523b97a631d507c4b07035425
uzay.durdu <uzay.durdu@uni-konstanz.de>
2023-10-24 12:43:13 +0200

    sqlerver-connection

--- DashboardHome.py
+++ DashboardHome.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from streamlit_option_menu import option_menu as om
-import mysql.connector 
+import mysql.connector
+import pyodbc as odbc
 
 #imports for EDA
 import pandas as pd
@@ -9,6 +10,8 @@ import plotly as plt
 
 #imports for ML
 import sklearn as sk
+
+
 
 st.set_page_config(page_title="DatAI", page_icon="icon.png", layout="wide")
 st.title("Dashboard")
@@ -38,55 +41,89 @@ if action == "Connect to my database":
 
         # Column 2: Database Action (Selectbox)
         with col2:
-            db_action = st.selectbox("Choose your database:", ["SQLite", "PostgreSQL", "MySQL"])
+            db_action = st.selectbox("Choose your database:", ["Microsoft SQL Server", "PostgreSQL", "MySQL"])
             db_host = st.text_input("Host")
             db_port = st.text_input("Port")
             
 
 
-        if db_action == "SQLite":
+        if db_action == "Microsoft SQL Server":
             # Add code to connect to SQLite database
-            st.write("You selected SQLite. Implement the connection code here.")
+            if st.button("Connect to Database"):
+                if db_user and db_password:
+                    try:
+                        connection_string = f"""
+                                DRIVER=ODBC Driver 17 for SQL Server;
+                                SERVER=DESKTOP-AR1L4FP\\SQLEXPRESS;
+                                DATABASE=Asptest;
+                                Trusted_Connection=yes;  
+                            """
+                        conn = odbc.connect(connection_string)
+                        st.write(conn)
+                        st.success("Database connection successful")
+                        
+                        
+                        cursor = conn.cursor()
+
+                        # Execute an SQL query to fetch data from the 'categories' table
+                        cursor.execute("SELECT * FROM categories;")
+                        
+                        # Fetch all rows
+                        rows = cursor.fetchall()
+
+                        # Display the data in a Streamlit table
+                        st.write("Data from 'categories' table:")
+                        for row in rows:
+                            st.write(f"{row[0]} has a :{row[1]}: {row[2]}")
+
+                      
+                        
+                        # You can add further logic to display data or perform database operations here
+
+                    except mysql.connector.Error as err:
+                        st.error(f"Database connection error: {err}")
+                else:
+                    st.error("Please enter username and password")
         elif db_action == "PostgreSQL":
             # Add code to connect to PostgreSQL database
             st.write("You selected PostgreSQL. Implement the connection code here.")
         elif db_action == "MySQL":
             # Add code to connect to MySQL database
-            st.write("You selected MySQL. Implement the connection code here.")
+            if st.button("Connect to Database"):
+                if db_user and db_password:
+                    try:
+                        connection = mysql.connector.connect(
+                            host="localhost",
+                            port="3306",
+                            user=str(db_user),  # Use the entered username
+                            password=str(db_password),  # Use the entered password
+                            database="sql_hr"
+                        )
+                        curs = connection.cursor()
+                        st.write(curs)
+                        st.success("Database connection successful")
+                        
+                        query = f"SELECT * FROM accounts"
+                        curs.execute(query)
+                        st.write(curs)
+                        data = curs.fetchall()
+                        df = pd.DataFrame(data, columns=["account_id", "account_name", "account_balance"])
+                        success_message.success("Database connection successful")
+                        db_successful_placeholder.dataframe(df) #st.dataframe(df)
+                        db_placeholder.empty()
+                        # You can add further logic to display data or perform database operations here
+
+                    except mysql.connector.Error as err:
+                        st.error(f"Database connection error: {err}")
+                else:
+                    st.error("Please enter username and password")
             
 
         
         
         
         # Button to connect to the database
-        if st.button("Connect to Database"):
-            if db_user and db_password:
-                try:
-                    connection = mysql.connector.connect(
-                        host="localhost",
-                        port="3306",
-                        user=str(db_user),  # Use the entered username
-                        password=str(db_password),  # Use the entered password
-                        database="sql_hr"
-                    )
-                    curs = connection.cursor()
-                    st.write(curs)
-                    st.success("Database connection successful")
-                    
-                    query = f"SELECT * FROM accounts"
-                    curs.execute(query)
-                    st.write(curs)
-                    data = curs.fetchall()
-                    df = pd.DataFrame(data, columns=["account_id", "account_name", "account_balance"])
-                    success_message.success("Database connection successful")
-                    db_successful_placeholder.dataframe(df) #st.dataframe(df)
-                    db_placeholder.empty()
-                    # You can add further logic to display data or perform database operations here
-
-                except mysql.connector.Error as err:
-                    st.error(f"Database connection error: {err}")
-            else:
-                st.error("Please enter username and password")
+        
                       
     
 elif action == "Upload Dataset":
