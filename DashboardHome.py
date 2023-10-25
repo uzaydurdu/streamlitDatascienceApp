9487280cf0302bd3e240c6031f7972510fc03d9b
uzay.durdu <uzay.durdu@uni-konstanz.de>
2023-10-25 23:17:41 +0200

    connections-tested

--- DashboardHome.py
+++ DashboardHome.py
@@ -16,7 +16,7 @@ default_postgresql_port = "5432"
 
 st.set_page_config(page_title="DatAI", page_icon="icon.png", layout="wide")
 st.title("Dashboard")
-st.text("Welcome to DatAI open-source tool for data science jobs.")
+st.text("Welcome to DatAI 💜 open-source tool for data science jobs.")
 
 action = st.selectbox("Choose an action", ["Connect to my database", "Upload Dataset"])
 st.markdown('<hr style="border: 1px solid lightgray;">', unsafe_allow_html=True)
@@ -42,7 +42,8 @@ if 'is_connected_micsql' not in st.session_state:
 if 'is_connected_pgsql' not in st.session_state:
     st.session_state.is_connected_pgsql = False  
 if 'is_connected_mysql' not in st.session_state:
-    st.session_state.is_connected_mysql = False            
+    st.session_state.is_connected_mysql = False 
+               
 
 def init_connections_mysql(dbhost, dbport, dbuser, dbpassword, dbname):
     try:
@@ -62,8 +63,8 @@ def init_connections_mysql(dbhost, dbport, dbuser, dbpassword, dbname):
         else:
             st.session_state.is_connected_mysql = True
 
-        with st.spinner('Connecting to Database. Please wait...'):
-            time.sleep(2)
+
+        
         success_conn = st.success("Database successfully connected.")
         time.sleep(2)
         success_conn.empty()
@@ -88,9 +89,9 @@ def init_connections_pgsql(dbhost, dbport, dbuser, dbpassword, dbname):
             st.session_state.is_connected_pgsql = True
         else:
             st.session_state.is_connected_pgsql = True
-        st.session_state
-        with st.spinner('Connecting to Database. Please wait...'):
-            time.sleep(2)
+        
+
+       
         success_conn = st.success("Database successfully connected.")
         time.sleep(2)
         success_conn.empty()  
@@ -116,8 +117,8 @@ def init_connections_micsql(server, name):
         else:
             st.session_state.is_connected_micsql = True
         
-        with st.spinner('Connecting to Database. Please wait...'):
-            time.sleep(2)
+
+        
         success_conn = st.success("Database successfully connected.")
         time.sleep(2)
         success_conn.empty()
@@ -158,8 +159,12 @@ def db_connection_layout():
         if connect_db_btn:
             if isMicSql:
                 if db_server and db_name:
+                    with st.spinner('Connecting to Database. Please wait...'):
+                        time.sleep(2)
                     conn = init_connections_micsql(db_server, db_name)
                     if 'db_conn' not in st.session_state:
+                        st.session_state['db_conn'] = conn
+                    else:
                         st.session_state['db_conn'] = conn
                         
                         return conn
@@ -168,21 +173,27 @@ def db_connection_layout():
                
             elif isPgSql:
                 if db_user and db_password and db_name:
+                    with st.spinner('Connecting to Database. Please wait...'):
+                        time.sleep(2)
                     conn = init_connections_pgsql(db_host, db_port, db_user, db_password, db_name)
 
                     if 'db_conn' not in st.session_state:
                         st.session_state['db_conn'] = conn
-                    
+                    else:
+                        st.session_state['db_conn'] = conn                    
                     return conn
                 else:
                      st.error("Please complete or correct your credentials.")
             elif isMySql:
                 if db_user and db_password and db_name and db_host and db_port:
+                    with st.spinner('Connecting to Database. Please wait...'):
+                        time.sleep(2)
                     conn = init_connections_mysql(db_host, db_port, db_user, db_password, db_name)
 
                     if 'db_conn' not in st.session_state:
                         st.session_state['db_conn'] = conn
-                    
+                    else:
+                        st.session_state['db_conn'] = conn
                     return conn
                 else:
                      st.error("Please complete or correct your credentials.")
@@ -219,7 +230,7 @@ if action == "Connect to my database":
         
 
         if selected_table:
-            with st.expander(f"Data for Table: {selected_table}"):
+            with st.expander(f"Data for Table: {selected_table}", expanded=True):
 
                 data_query = f"SELECT * FROM {selected_table};"
                 cursor.execute(data_query)
@@ -256,10 +267,47 @@ if action == "Connect to my database":
         columns = cursor.fetchall()
         
         column_names = [column[0] for column in columns]
-        st.write(column_names)
 
         if selected_table:
-            with st.expander(f"Data for Table: {selected_table}"):
+            with st.expander(f"Data for Table: {selected_table}", expanded=True):
+
+                data_query = f"SELECT * FROM {selected_table};"
+                cursor.execute(data_query)
+                table_data = cursor.fetchall()
+                #st.write(list(table_data[0]))
+                data = [list(d) for d in table_data]
+
+
+                if table_data:
+                    table_data_df = pd.DataFrame(data, columns=column_names)
+                    st.dataframe(table_data_df)
+
+    elif st.session_state.is_connected_mysql:
+        db_placeholder.empty()
+        
+        conn = st.session_state.db_conn
+        cursor = conn.cursor()
+
+        # Execute an SQL query to fetch data from the 'categories' table
+        table_query = "SHOW TABLES"
+        cursor.execute(table_query)
+
+        # Fetch all rows
+        tables = cursor.fetchall()
+        table_names = [table[0] for table in tables]
+        
+        selected_table = st.selectbox("Select a table:", table_names)
+
+        column_query = f"DESCRIBE {selected_table}"
+        
+        cursor.execute(column_query)
+        #column_names = [desc[0] for desc in cursor.description]
+        columns = cursor.fetchall()
+        
+        column_names = [column[0] for column in columns]
+
+        if selected_table:
+            with st.expander(f"Data for Table: {selected_table}", expanded=True):
 
                 data_query = f"SELECT * FROM {selected_table};"
                 cursor.execute(data_query)
