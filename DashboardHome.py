a26811411597556d308f838b526b6e53f68912ff
uzay.durdu <uzay.durdu@uni-konstanz.de>
2023-10-25 21:19:51 +0200

    postgresql-connected

--- DashboardHome.py
+++ DashboardHome.py
@@ -1,17 +1,18 @@
 import streamlit as st
-from streamlit_option_menu import option_menu as om
 import mysql.connector
 import pyodbc as odbc
-
-#imports for EDA
+import psycopg2
 import pandas as pd
-import numpy as np
-import plotly as plt
+import time
 
-#imports for ML
-import sklearn as sk
+# Initialize your database connection
+isMicSql = False
+isPgSql = False
+isMySql = False
+tables = []  # Initialize an empty list for tables
 
-
+default_postgresql_host = "localhost"
+default_postgresql_port = "5432"
 
 st.set_page_config(page_title="DatAI", page_icon="icon.png", layout="wide")
 st.title("Dashboard")
@@ -21,117 +22,256 @@ action = st.selectbox("Choose an action", ["Connect to my database", "Upload Dat
 st.markdown('<hr style="border: 1px solid lightgray;">', unsafe_allow_html=True)
 
 db_placeholder = st.empty()
+db_table_selectbox = st.empty()
 db_successful_placeholder = st.empty()
 success_message = st.empty()
 
-with st.sidebar:
-    st.write("This is your sidebar content")
+# Database connection details
+db_host = None
+db_port = None
+db_user = None
+db_password = None
+db_name = None
+db_server = None
+conn = None
+
+if 'is_table' not in st.session_state:
+    st.session_state.is_table = False
+if 'is_connected_micsql' not in st.session_state:
+    st.session_state.is_connected_micsql = False
+if 'is_connected_pgsql' not in st.session_state:
+    st.session_state.is_connected_pgsql = False  
+if 'is_connected_mysql' not in st.session_state:
+    st.session_state.is_connected_mysql = False            
+
+def init_connections_mysql(dbhost, dbport, dbuser, dbpassword, dbname):
+    try:
+        connection = mysql.connector.connect(
+            host=dbhost,
+            port=dbport,
+            user=dbuser,
+            password=dbpassword,
+            database=dbname
+        )
+        if 'is_table' not in st.session_state:
+            st.session_state.is_table = True
+        else:
+            st.session_state.is_table = True
+        if 'is_connected_mysql' not in st.session_state:
+            st.session_state.is_connected_mysql = True
+        else:
+            st.session_state.is_connected_mysql = True
+
+        with st.spinner('Connecting to Database. Please wait...'):
+            time.sleep(2)
+        success_conn = st.success("Database successfully connected.")
+        time.sleep(2)
+        success_conn.empty()
+        return connection
+    except mysql.connector.Error as err:
+        st.error(f"Database connection error: {err}")
+
+def init_connections_pgsql(dbhost, dbport, dbuser, dbpassword, dbname):
+    try:
+        connection = psycopg2.connect(
+            host=dbhost,
+            port=dbport,
+            user=dbuser,
+            password=dbpassword,
+            database=dbname
+        )
+        if 'is_table' not in st.session_state:
+            st.session_state.is_table = True
+        else:
+            st.session_state.is_table = True
+        if 'is_connected_pgsql' not in st.session_state:
+            st.session_state.is_connected_pgsql = True
+        else:
+            st.session_state.is_connected_pgsql = True
+        st.session_state
+        with st.spinner('Connecting to Database. Please wait...'):
+            time.sleep(2)
+        success_conn = st.success("Database successfully connected.")
+        time.sleep(2)
+        success_conn.empty()  
+        return connection
+    except psycopg2.Error as err:
+        st.error(f"Database connection error: {err}")
+
+def init_connections_micsql(server, name):
+    try:
+        connection_string = f"""
+            DRIVER=ODBC Driver 17 for SQL Server;
+            SERVER={{{server}}};
+            DATABASE={{{name}}};
+            Trusted_Connection=yes;
+        """
+        conn = odbc.connect(connection_string)
+        if 'is_table' not in st.session_state:
+            st.session_state.is_table = True
+        else:
+            st.session_state.is_table = True
+        if 'is_connected_micsql' not in st.session_state:
+            st.session_state.is_connected_micsql = True
+        else:
+            st.session_state.is_connected_micsql = True
+        
+        with st.spinner('Connecting to Database. Please wait...'):
+            time.sleep(2)
+        success_conn = st.success("Database successfully connected.")
+        time.sleep(2)
+        success_conn.empty()
+        return conn
+    except odbc.Error as err:
+        st.error(f"Database connection error: {err}")
+        st.session_state.is_connected = False
+        st.session_state.is_table = False
+
+def db_connection_layout():
+    with db_placeholder.container():
+        st.subheader('Database Connection')
+        left_credentials, right_credentials = st.columns(2)
+        right_credentials.selectbox("Choose your database:", ["Microsoft SQL Server", "PostgreSQL", "MySQL"], key="selected_db")
+        
+        isMicSql = True if st.session_state.selected_db == "Microsoft SQL Server" else False
+        isPgSql = True if st.session_state.selected_db == "PostgreSQL" else False
+        isMySql = True if st.session_state.selected_db == "MySQL" else False
+
+        if isMicSql:
+            db_server = left_credentials.text_input("Server Name")
+            db_name = left_credentials.text_input("Database Name")
+        elif isPgSql:
+            db_user = left_credentials.text_input("Database Username")
+            db_password = left_credentials.text_input("Database Password", type="password")
+            db_name = left_credentials.text_input("Database Name")
+            db_host = right_credentials.text_input("Host", default_postgresql_host)
+            db_port = right_credentials.text_input("Port", default_postgresql_port)
+        elif isMySql:
+            db_user = left_credentials.text_input("Database Username")
+            db_password = left_credentials.text_input("Database Password", type="password")
+            db_name = left_credentials.text_input("Database Name")
+            db_host = right_credentials.text_input("Host")
+            db_port = right_credentials.text_input("Port")
+        
+        connect_db_btn = st.button("Connect Database")
+        
+        if connect_db_btn:
+            if isMicSql:
+                if db_server and db_name:
+                    conn = init_connections_micsql(db_server, db_name)
+                    if 'db_conn' not in st.session_state:
+                        st.session_state['db_conn'] = conn
+                        
+                        return conn
+                else:
+                    st.error("Please enter server and database name.")
+               
+            elif isPgSql:
+                if db_user and db_password and db_name:
+                    conn = init_connections_pgsql(db_host, db_port, db_user, db_password, db_name)
+
+                    if 'db_conn' not in st.session_state:
+                        st.session_state['db_conn'] = conn
+                    
+                    return conn
+                else:
+                     st.error("Please complete or correct your credentials.")
+            elif isMySql:
+                if db_user and db_password and db_name and db_host and db_port:
+                    conn = init_connections_mysql(db_host, db_port, db_user, db_password, db_name)
+
+                    if 'db_conn' not in st.session_state:
+                        st.session_state['db_conn'] = conn
+                    
+                    return conn
+                else:
+                     st.error("Please complete or correct your credentials.")
+    
+
+def upload_dataset_layout():
+    uploaded_file = st.file_uploader("Upload your dataset file", type=["csv", "txt", "json"])
 
 if action == "Connect to my database":
     
-    with db_placeholder.container():
+    db_connection_layout()
+    if st.session_state.is_connected_micsql:
+        db_placeholder.empty()
+        
+        conn = st.session_state.db_conn
+        cursor = conn.cursor()
 
-        col1, col2 = st.columns(2)
+        # Execute an SQL query to fetch data from the 'categories' table
+        table_query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE';"
+        cursor.execute(table_query)
 
-        # Column 1: Database User and Password
-        with col1:
-            db_user = st.text_input("Database Username")
-            db_password = st.text_input("Database Password", type="password")
-            db_name = st.text_input("Database Name")
-
-        # Column 2: Database Action (Selectbox)
-        with col2:
-            db_action = st.selectbox("Choose your database:", ["Microsoft SQL Server", "PostgreSQL", "MySQL"])
-            db_host = st.text_input("Host")
-            db_port = st.text_input("Port")
-            
-
-
-        if db_action == "Microsoft SQL Server":
-            # Add code to connect to SQLite database
-            if st.button("Connect to Database"):
-                if db_user and db_password:
-                    try:
-                        connection_string = f"""
-                                DRIVER=ODBC Driver 17 for SQL Server;
-                                SERVER=DESKTOP-AR1L4FP\\SQLEXPRESS;
-                                DATABASE=Asptest;
-                                Trusted_Connection=yes;  
-                            """
-                        conn = odbc.connect(connection_string)
-                        st.write(conn)
-                        st.success("Database connection successful")
-                        
-                        
-                        cursor = conn.cursor()
-
-                        # Execute an SQL query to fetch data from the 'categories' table
-                        cursor.execute("SELECT * FROM categories;")
-                        
-                        # Fetch all rows
-                        rows = cursor.fetchall()
-
-                        # Display the data in a Streamlit table
-                        st.write("Data from 'categories' table:")
-                        for row in rows:
-                            st.write(f"{row[0]} has a :{row[1]}: {row[2]}")
-
-                      
-                        
-                        # You can add further logic to display data or perform database operations here
-
-                    except mysql.connector.Error as err:
-                        st.error(f"Database connection error: {err}")
-                else:
-                    st.error("Please enter username and password")
-        elif db_action == "PostgreSQL":
-            # Add code to connect to PostgreSQL database
-            st.write("You selected PostgreSQL. Implement the connection code here.")
-        elif db_action == "MySQL":
-            # Add code to connect to MySQL database
-            if st.button("Connect to Database"):
-                if db_user and db_password:
-                    try:
-                        connection = mysql.connector.connect(
-                            host="localhost",
-                            port="3306",
-                            user=str(db_user),  # Use the entered username
-                            password=str(db_password),  # Use the entered password
-                            database="sql_hr"
-                        )
-                        curs = connection.cursor()
-                        st.write(curs)
-                        st.success("Database connection successful")
-                        
-                        query = f"SELECT * FROM accounts"
-                        curs.execute(query)
-                        st.write(curs)
-                        data = curs.fetchall()
-                        df = pd.DataFrame(data, columns=["account_id", "account_name", "account_balance"])
-                        success_message.success("Database connection successful")
-                        db_successful_placeholder.dataframe(df) #st.dataframe(df)
-                        db_placeholder.empty()
-                        # You can add further logic to display data or perform database operations here
-
-                    except mysql.connector.Error as err:
-                        st.error(f"Database connection error: {err}")
-                else:
-                    st.error("Please enter username and password")
-            
+        # Fetch all rows
+        tables = cursor.fetchall()
+        table_names = [table[0] for table in tables]
 
         
+        selected_table = st.selectbox("Select a table:", table_names)
+
+        column_query = f"SELECT * FROM INFORMATION_SCHEMA.COLUMNS WHERE TABLE_NAME = N'{selected_table}';"
+        cursor.execute(column_query)
+        #column_names = [desc[0] for desc in cursor.description]
+        columns = cursor.fetchall()
+        column_names = [column[3] for column in columns]
         
+
+        if selected_table:
+            with st.expander(f"Data for Table: {selected_table}"):
+
+                data_query = f"SELECT * FROM {selected_table};"
+                cursor.execute(data_query)
+                table_data = cursor.fetchall()
+                #st.write(list(table_data[0]))
+                data = [list(d) for d in table_data]
+
+
+                if table_data:
+                    table_data_df = pd.DataFrame(data, columns=column_names)
+                    st.dataframe(table_data_df)
+
+    elif st.session_state.is_connected_pgsql:
+        db_placeholder.empty()
         
-        # Button to connect to the database
+        conn = st.session_state.db_conn
+        cursor = conn.cursor()
+
+        # Execute an SQL query to fetch data from the 'categories' table
+        table_query = "SELECT table_name FROM information_schema.tables WHERE table_schema = 'public' ORDER BY table_name;"
+        cursor.execute(table_query)
+
+        # Fetch all rows
+        tables = cursor.fetchall()
+        table_names = [table[0] for table in tables]
+
         
-                      
-    
+        selected_table = st.selectbox("Select a table:", table_names)
+
+        column_query = f"SELECT column_name FROM information_schema.columns WHERE table_name = '{selected_table}'"
+        
+        cursor.execute(column_query)
+        #column_names = [desc[0] for desc in cursor.description]
+        columns = cursor.fetchall()
+        
+        column_names = [column[0] for column in columns]
+        st.write(column_names)
+
+        if selected_table:
+            with st.expander(f"Data for Table: {selected_table}"):
+
+                data_query = f"SELECT * FROM {selected_table};"
+                cursor.execute(data_query)
+                table_data = cursor.fetchall()
+                #st.write(list(table_data[0]))
+                data = [list(d) for d in table_data]
+
+
+                if table_data:
+                    table_data_df = pd.DataFrame(data, columns=column_names)
+                    st.dataframe(table_data_df)
+
 elif action == "Upload Dataset":
-    # File uploader for dataset upload
-    uploaded_file = st.file_uploader("Upload your dataset file", type=["csv", "txt", "json"])
-
-    if uploaded_file is not None:
-        # Process the uploaded dataset
-        st.write(f"File uploaded: {uploaded_file.name}")
-
-# Define a function to display table rows (you can call this function as needed)
+    st.write("You selected dataset")
+    upload_dataset_layout()
--- MySQLConnector.py
+++ MySQLConnector.py
@@ -18,3 +18,130 @@ def display_table_rows():
     data = curs.fetchall()
     return data
 
+def display_tables(conn):
+    cursor = conn.cursor()
+
+    # Execute an SQL query to fetch data from the 'categories' table
+    table_query = "SELECT table_name FROM information_schema.tables WHERE table_type = 'BASE TABLE';"
+    cursor.execute(table_query)
+
+    # Fetch all rows
+    tables = cursor.fetchall()
+    table_names = [table[0] for table in tables]
+
+    
+    
+    # Use st.selectbox for table selection
+    selected_table = st.selectbox("Select a table:", table_names)
+
+    if selected_table:
+        with st.expander(f"Data for Table: {selected_table}"):
+            # Define a function to load table data dynamically
+            data_query = f"SELECT * FROM {selected_table};"
+            cursor.execute(data_query)
+            table_data = cursor.fetchall()
+
+            # Create a DataFrame from the data
+            if table_data:
+                table_data_df = pd.DataFrame(table_data)
+                st.dataframe(table_data_df)
+
+def init_connections_mysql():
+    try:
+        connection = mysql.connector.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        return connection
+    except mysql.connector.Error as err:
+        st.error(f"Database connection error: {err}")
+
+def init_connections_pgsql():
+    try:
+        connection = psycopg2.connect(
+            host=db_host,
+            port=db_port,
+            user=db_user,
+            password=db_password,
+            database=db_name
+        )
+        return connection
+    except psycopg2.Error as err:
+        st.error(f"Database connection error: {err}")
+
+def init_connections_micsql(server, name):
+    try:
+        connection_string = f"""
+            DRIVER=ODBC Driver 17 for SQL Server;
+            SERVER={{{server}}};
+            DATABASE={{{name}}};
+            Trusted_Connection=yes;
+        """
+        conn = odbc.connect(connection_string)
+        return conn
+    except odbc.Error as err:
+        st.error(f"Database connection error: {err}")
+
+def db_form():
+    with db_placeholder.container():
+        st.subheader('Connect Database')
+        col1, col2 = st.columns(2)
+        with col2:
+            db_action = st.selectbox("Choose your database:", ["Microsoft SQL Server", "PostgreSQL", "MySQL"])
+            if db_action != "Microsoft SQL Server" and db_action != "PostgreSQL":
+                db_host = st.text_input("Host")
+                db_port = st.text_input("Port")
+            if db_action == "PostgreSQL":
+                db_host = st.text_input("Host", default_postgresql_host)
+                db_port = st.text_input("Port", default_postgresql_port)
+        # Column 1: Database User and Password
+        with col1:
+            if db_action != "Microsoft SQL Server":
+                db_user = st.text_input("Database Username")
+                db_password = st.text_input("Database Password", type="password")
+                db_name = st.text_input("Database Name")
+            else:
+                db_server = st.text_input("Server Name")
+                db_name = st.text_input("Database Name")
+        conn_button = st.button("Connect Database")
+        if conn_button:
+            if db_action == "Microsoft SQL Server":
+                if db_server and db_name:
+                    conn = init_connections_micsql(db_server, db_name)
+                    st.write("Connection successful")
+                    return conn
+                else:
+                    st.error("Please enter server and database name.")
+            elif db_action == "PostgreSQL":
+                if db_user and db_password and db_name and db_host:
+                    conn = init_connections_pgsql()
+                    display_tables(conn)
+                    st.write("Connection successful")
+                else:
+                    st.error("Please enter all required information.")
+            elif db_action == "MySQL":
+                if db_user and db_password and db_name and db_host and db_port:
+                    conn = init_connections_mysql()
+                    display_tables(conn)
+                    st.write("Connection successful")
+                else:
+                    st.error("Please enter all required information.")
+
+with st.sidebar:
+    st.write("This is your sidebar content")
+
+if action == "Connect to my database":
+    conn = db_form()
+    if conn:
+        display_tables(conn)
+
+elif action == "Upload Dataset":
+    # File uploader for dataset upload
+    uploaded_file = st.file_uploader("Upload your dataset file", type=["csv", "txt", "json"])
+
+    if uploaded_file is not None:
+        # Process the uploaded dataset
+        st.write(f"File uploaded: {uploaded_file.name}")
